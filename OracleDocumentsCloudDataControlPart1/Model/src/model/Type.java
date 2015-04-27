233c65201921b48e5e84da791c3dc31e02305ae4
Justin Paul <JustinPaul@justins-mbp.fios-router.home>
2015-04-26 22:48:42 -0400

    Oracle Documents Cloud Data Control - Part 1

--- OracleDocumentsCloudDataControlPart1/Model/src/model/Type.java
+++ OracleDocumentsCloudDataControlPart1/Model/src/model/Type.java
@@ -1,3 +1,11 @@
+/*##############################################################################
+# Created By: Justin Paul
+# Source:     https://blogs.oracle.com/OracleWebCenterSuite
+#
+# NOTE: Please note that these code snippets should be used for development and
+#       testing purposes only, as such it is unsupported and should not be used
+#       on production environments.
+##############################################################################*/
 package model;
 
 public enum Type {
