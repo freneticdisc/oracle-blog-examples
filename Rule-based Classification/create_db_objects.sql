6acac2a98d7b6021fc36741951474649e29f715e
Justin Paul <JustinPaul@Justins-MBP.fios-router.home>
2015-05-07 22:15:55 -0400

    Rule-based Classification in WebCenter Content
    Rule-based Classification using Oracle Text in WebCenter Content

--- Rule-based Classification/create_db_objects.sql
+++ Rule-based Classification/create_db_objects.sql
@@ -8,55 +8,55 @@ INDEXTYPE
        OR
   REPLACE
  FUNCTION dev1_ocs.categorizeDocument(
- 		     ddocname IN VARCHAR2,
- 		     activeindex IN VARCHAR2)
+             ddocname IN VARCHAR2,
+             activeindex IN VARCHAR2)
    RETURN VARCHAR2
        IS
-       		b_docfulltext	BLOB;
-         	c_document    	CLOB;
-			i_dest_offset   INTEGER := 1;
-			i_src_offset    INTEGER := 1;
-			i_lang_context  INTEGER := dbms_lob.default_lang_ctx;
-			i_warning     	INTEGER;
-			v_categories	VARCHAR2(200) := '';
-			v_query			VARCHAR2(100) := 'SELECT ';
+            b_docfulltext   BLOB;
+            c_document      CLOB;
+            i_dest_offset   INTEGER := 1;
+            i_src_offset    INTEGER := 1;
+            i_lang_context  INTEGER := dbms_lob.default_lang_ctx;
+            i_warning       INTEGER;
+            v_categories    VARCHAR2(200) := '';
+            v_query         VARCHAR2(100) := 'SELECT ';
     BEGIN
-    		    IF (INSTR(activeindex, 'IDCCOLL') = 1)
-    		  THEN
-    			   v_query := v_query || 'ddocfulltext';
-    		 ELSIF (INSTR(activeindex, 'IDCTEXT') = 1)
-    		  THEN
-    		 	   v_query := v_query || 'otscontent';
-    		  ELSE
-    		  	   v_query := v_query || '''X''';
-    		END IF;
+                IF (INSTR(activeindex, 'IDCCOLL') = 1)
+              THEN
+                   v_query := v_query || 'ddocfulltext';
+             ELSIF (INSTR(activeindex, 'IDCTEXT') = 1)
+              THEN
+                   v_query := v_query || 'otscontent';
+              ELSE
+                   v_query := v_query || '''X''';
+            END IF;
 
-    		v_query := v_query || '  FROM ' || activeindex;
-    		v_query := v_query || ' WHERE ddocname = :1';
-    	 	dbms_lob.createtemporary(c_document, true);
-    	 	  EXECUTE
-    	 	IMMEDIATE v_query
-    	 	     INTO b_docfulltext
-    	 	    USING ddocname;
-    	 	dbms_lob.converttoclob(c_document,
-                          		   b_docfulltext,
-                          		   dbms_lob.lobmaxsize,
-                          		   i_dest_offset,
-                          		   i_src_offset,
-                          		   dbms_lob.default_csid,
-                          		   i_lang_context,
-                          		   i_warning);
+            v_query := v_query || '  FROM ' || activeindex;
+            v_query := v_query || ' WHERE ddocname = :1';
+            dbms_lob.createtemporary(c_document, true);
+              EXECUTE
+            IMMEDIATE v_query
+                 INTO b_docfulltext
+                USING ddocname;
+            dbms_lob.converttoclob(c_document,
+                                   b_docfulltext,
+                                   dbms_lob.lobmaxsize,
+                                   i_dest_offset,
+                                   i_src_offset,
+                                   dbms_lob.default_csid,
+                                   i_lang_context,
+                                   i_warning);
             FOR cat IN (SELECT categoryid FROM classificationcategories WHERE MATCHES(categoryselectionrule, c_document) > 0)
             LOOP
-            	v_categories := v_categories || cat.categoryid || ', ';
+                v_categories := v_categories || cat.categoryid || ', ';
             END LOOP;
             dbms_lob.freetemporary(c_document);
             v_categories := TRIM(TRAILING ' ' FROM v_categories);
             v_categories := TRIM(TRAILING ',' FROM v_categories);
             RETURN v_categories;
 EXCEPTION
-		 	WHEN OTHERS
-		 	THEN RETURN '';
+            WHEN OTHERS
+            THEN RETURN '';
       END;
 
 /
--- Rule-based Classification/generate_categories_example.sql
+++ Rule-based Classification/generate_categories_example.sql
@@ -1,25 +1,25 @@
 DECLARE
-		v_cats VARCHAR2(200) := '';
-		v_indx VARCHAR2(8)	  := 'IDCCOLL2';
+        v_cats VARCHAR2(200) := '';
+        v_indx VARCHAR2(8)    := 'IDCCOLL2';
  CURSOR content_released_today
      IS
         SELECT did,
-        	   ddocname
+               ddocname
           FROM revisions
          WHERE TRUNC(dreleasedate) = TRUNC(SYSDATE);
   BEGIN
-  		 	  FOR content IN content_released_today
-  		     LOOP
-  		     	  v_cats := dev1_ocs.categorizeDocument(content.ddocname, v_indx);
-  		     	      IF NVL(v_cats, 'NULL') <> 'NULL'
-  		     	    THEN
-  		     	  		 UPDATE dev1_ocs.docmeta
-  		     	     		SET xclassificationcategory = v_cats
-  		     	   		  WHERE did = content.did;
-  		     	  		 UPDATE dev1_ocs.revisions
-  		     	     		SET dreleasestate = 'U'
-  		     	   		  WHERE did = content.did;
-  		     	  END IF;
-  		 END LOOP;
-  		   COMMIT;
+              FOR content IN content_released_today
+             LOOP
+                  v_cats := dev1_ocs.categorizeDocument(content.ddocname, v_indx);
+                      IF NVL(v_cats, 'NULL') <> 'NULL'
+                    THEN
+                         UPDATE dev1_ocs.docmeta
+                            SET xclassificationcategory = v_cats
+                          WHERE did = content.did;
+                         UPDATE dev1_ocs.revisions
+                            SET dreleasestate = 'U'
+                          WHERE did = content.did;
+                  END IF;
+         END LOOP;
+           COMMIT;
     END;
